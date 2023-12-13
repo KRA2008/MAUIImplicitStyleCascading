d503c1533f3e973c293393336286d41030bf2784
Kevin Anderson <kraatw2008@yahoo.com>
2023-12-12 20:02:28 -0600

    hello

---
+++ ExampleApp/InnerPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace ExampleApp;
+
+public partial class InnerPage
+{
+	public InnerPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ExampleApp/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace ExampleApp
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_OnClicked(object? sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new InnerPage());
+        }
+    }
+
+}
